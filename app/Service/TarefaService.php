cd0a070ef34d2c8fa27aaa42b1e8b81b99ed55d7
Gabrielasaraiv <gabrielasaraivaaraujo358@gmail.com>
2024-09-13 09:21:51 -0300

    modified:   app/Service/TarefaService.php 	modified:   routes/api.php 	modified:   composer.lock

--- app/Service/TarefaService.php
+++ app/Service/TarefaService.php
@@ -21,5 +21,3 @@ class TarefaService
         ];
     }
 }
-
-//teste
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/tarefa', [TarefaController::class, 'store']);
+
+//teste
